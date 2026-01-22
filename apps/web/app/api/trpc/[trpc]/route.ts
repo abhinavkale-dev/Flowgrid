d97dee91530785bb33414ae16f1a00bc3a232554
Abhi16016 <abhinavkale919913@gmail.com>
2026-01-22 23:15:04 +0530

    trpc client side

---
+++ apps/web/app/api/trpc/[trpc]/route.ts
@@ -0,0 +1,15 @@
+import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
+import { appRouter } from '@/server/routers/_app';
+import { createContext } from '@/server/trpc';
+
+
+const handler = (req: Request) => {
+    return fetchRequestHandler({
+        endpoint: '/api/trpc',
+        req,
+        router: appRouter,
+        createContext,
+    });
+};
+
+export { handler as GET, handler as POST };
